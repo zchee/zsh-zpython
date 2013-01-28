c8bc5ac1d74442999b03c574c9ef7b055c9a75b4
ZyX <kp-pav@ya.ru>
2013-01-28 20:44:24 +0400

    Added partial support for zsh.getvalue (obtains variable value) Still to support: arrays and hashes.

--- Src/Modules/zpython.c
+++ Src/Modules/zpython.c
@@ -80,6 +80,68 @@ ZshEval(UNUSED(PyObject *self), PyObject *args)
     Py_RETURN_NONE;
 }
 
+static PyObject *
+ZshGetValue(UNUSED(PyObject *self), PyObject *args)
+{
+    char *name;
+    struct value vbuf;
+    Value v;
+
+    if(!PyArg_ParseTuple(args, "s", &name))
+        return NULL;
+
+    if(!isident(name)) {
+        PyErr_SetString(PyExc_ValueError, "Parameter name is not an identifier");
+        return NULL;
+    }
+
+    if(!(v = getvalue(&vbuf, &name, 1))) {
+        PyErr_SetString(PyExc_KeyError, "Failed to find parameter");
+        return NULL;
+    }
+
+    switch(PM_TYPE(v->pm->node.flags)) {
+        case PM_HASHED:
+        case PM_ARRAY:
+            PyErr_SetString(PyExc_NotImplementedError, "Hashes and arrays are currently not supported");
+            return NULL;
+        case PM_INTEGER:
+            return PyLong_FromLong((long) v->pm->gsu.i->getfn(v->pm));
+        case PM_EFLOAT:
+        case PM_FFLOAT:
+            return PyFloat_FromDouble(v->pm->gsu.f->getfn(v->pm));
+        case PM_SCALAR:
+            {
+            char *s, *buf, *bufstart;
+            PyObject *r;
+            s = v->pm->gsu.s->getfn(v->pm);
+            /* No need in \0 byte at the end since we are using
+             * PyString_FromStringAndSize */
+            buf = PyMem_New(char, strlen(s));
+            bufstart = buf;
+            while (*s) {
+                *buf++ = (*s == Meta) ? (*++s ^ 32) : (*s);
+                ++s;
+            }
+            r = PyString_FromStringAndSize(bufstart, (Py_ssize_t)(buf - bufstart));
+            PyMem_Free(bufstart);
+            return r;
+            }
+        default:
+            PyErr_SetString(PyExc_SystemError, "Parameter has unknown type; should not happen.");
+            return NULL;
+    }
+}
+
+/*
+ * createparam, createspecialhash
+ *
+ * isident for finding ValueErrors
+ *
+ * getiparam, !!!getnparam, getsparam, getaparam, gethparam
+ * gethkparam
+ */
+
 #define ZSH_GETLONG_FUNCTION(funcname, var) \
 static PyObject * \
 Zsh##funcname(UNUSED(PyObject *self), UNUSED(PyObject *args)) \
@@ -98,6 +160,7 @@ static struct PyMethodDef ZshMethods[] = {
     {"columns", ZshColumns, 0, "Get number of columns"},
     {"lines", ZshLines, 0, "Get number of lines"},
     {"subshell", ZshSubshell, 0, "Get subshell recursion depth"},
+    {"getvalue", ZshGetValue, 1, "Get parameter value"},
     {NULL, NULL, 0, NULL},
 };
 
